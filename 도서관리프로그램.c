58e544aa1dd96091cae03584887f6b22bc8636ed
MoonJungHyeok <wjdgur9534@daum.net>
2020-11-28 15:29:43 +0900

    도서관리프로그램 구조체로 리팩토링

--- 도서관리프로그램.c
+++ 도서관리프로그램.c
@@ -1,17 +1,30 @@
 #include <stdio.h>
+typedef struct BookInformation{
+    char book_name[30];
+    char auth_name[30];
+    char publ_name[30];
+    int borrowed;
+}BookInformation;
 int add_book(char (*book_name)[30], char(*auth_name)[30], char(*publ_name)[30],int * borrowed, int * book_total_num);
+int register_book(BookInformation * library,int * book_total_num);
 int search_book(char (*book_name)[30], char(*auth_name)[30], char(*publ_name)[30],int * borrowed,int book_total_num);
+int search_book_struct(BookInformation * library,int book_total_num);
 int borrow_book(int * borrowed,char (*book_name)[30], char(*auth_name)[30], char(*publ_name)[30]);
+int borrow_book_struct(BookInformation * library);
 int return_book(int * borrowed,char (*book_name)[30], char(*auth_name)[30], char(*publ_name)[30]);
+int return_book_struct(BookInformation * library);
 int contain(char * str1, char *str2);
 void search_book_helper(char (*searchInfo)[30], char * user_input,int book_total_num,int * borrwowed);
+// void search_book_helper(char (*searchInfo)[30], char * user_input,int book_total_num,BookInformation * library);
+void search_book_helper_struct(BookInformation * library, int user_choice, char * user_input,int book_total_num);
+void print_book_list(BookInformation * library,int book_total_num);
 int strLen(char * str);
-
 int main() {
     int user_choice; /* 유저가 선택한 메뉴*/
-    char book_name[100][30], auth_name[100][30], publ_name[100][30];
-    int borrowed[100], book_total_num=0;
-    printf("%d \n",book_total_num);
+    // char book_name[100][30], auth_name[100][30], publ_name[100][30];
+    // int borrowed[100],
+    BookInformation library[100];
+    int book_total_num=0;
     while (1) {
         printf("도서 관리 프로그램 \n");
         printf("메뉴를 선택하세요 \n");
@@ -19,23 +32,32 @@ int main() {
         printf("2. 책을 검색하기 \n");
         printf("3. 책을 빌리기 \n");
         printf("4. 책을 반납하기 \n");
-        printf("5. 프로그램 종료 \n");
+        printf("5. 책 리스트 출력 \n");
+        printf("6. 프로그램 종료\n");
+
 
         printf("당신의 선택은 : ");
         scanf("%d", &user_choice);
         if (user_choice == 1) {
             /*책을 추가하는 함수 호출*/
-            add_book(book_name,auth_name,publ_name,borrowed,&book_total_num);
+            register_book(library,&book_total_num);
         } else if (user_choice == 2) {
             /* 책을 검색하는 함수 호출 */
-            search_book(book_name,auth_name,publ_name,borrowed,book_total_num);
+            // search_book(book_name,auth_name,publ_name,borrowed,book_total_num);
+            search_book_struct(library,book_total_num);
         } else if (user_choice == 3) {
             /* 책을 빌리는 함수 호출 */
-            borrow_book(borrowed,book_name,auth_name,publ_name);
+            // borrow_book(borrowed,book_name,auth_name,publ_name);
+            borrow_book_struct(library);
         } else if (user_choice == 4) {
             /* 책을 반납하는 함수 호출 */
-            return_book(borrowed,book_name,auth_name,publ_name);
+            // return_book(borrowed,book_name,auth_name,publ_name);
+            return_book_struct(library);
         } else if (user_choice == 5) {
+            /* 책 목록 전체 출력 */
+            print_book_list(library, book_total_num);
+            
+        }else if(user_choice==6){
             /* 프로그램을 종료한다. */
             break;
         }
@@ -43,6 +65,18 @@ int main() {
 
   return 0;
 }
+void print_book_list(BookInformation * library,int book_total_num){
+    printf("책 전체 목록을 출력합니다.\n");
+    int idx=0;
+    for(idx=0; idx<book_total_num; idx++){
+        printf("책의 번호 %d // 책의 이름 %s // 책 출판사 %s // 책 출판사 %s", idx,library[idx].book_name,library[idx].auth_name,library[idx].publ_name);
+        if(library[idx].borrowed==1)
+            printf(" 대출됨\n");
+        else
+            printf(" \n");
+    }
+}
+
 //책 빌리는 함수
 int borrow_book(int * borrowed,char (*book_name)[30], char(*auth_name)[30], char(*publ_name)[30]){
     int book_num;
@@ -57,16 +91,38 @@ int borrow_book(int * borrowed,char (*book_name)[30], char(*auth_name)[30], char
     }
     return 0;
 }
+int borrow_book_struct(BookInformation * library){
+    int book_num;
+    printf("빌릴 책의 번호를 입력하세요 :");
+    scanf("%d", &book_num);
+    if(library[book_num].borrowed==1)
+        printf("이미 대출된 책입니다. \n");
+    else{
+        library[book_num].borrowed=1;
+        printf("성공적으로 대출했습니다. 대출한 책의 번호는 %d // 책의 이름은 %s // 책의 저자는 %s // 책의 출판사는 %s 입니다.\n",book_num,library[book_num].book_name,library[book_num].auth_name,library[book_num].publ_name);
+    }
+
+    return 0;
+}
+
 
 int return_book(int * borrowed,char (*book_name)[30], char(*auth_name)[30], char(*publ_name)[30]){
     int book_num;
     printf("반납할 책의 번호를 입력하세요 : ");
     scanf("%d", &book_num);
     borrowed[book_num]=0;
-    printf("성공적으로 반납했습니다. 대출한 책의 번호는 %d // 책의 이름은 %s // 책의 저자는 %s // 책의 출판사는 %s 입니다.\n",book_num,book_name[book_num],auth_name[book_num],publ_name[book_num]);
+    printf("성공적으로 반납했습니다. 반납한 책의 번호는 %d // 책의 이름은 %s // 책의 저자는 %s // 책의 출판사는 %s 입니다.\n",book_num,book_name[book_num],auth_name[book_num],publ_name[book_num]);
     return 0;
 }
-//serach_book 리팩토링을 위한함수
+int return_book_struct(BookInformation * library){
+    int book_num;
+    printf("반납할 책의 번호를 입력하세요 : ");
+    scanf("%d", &book_num);
+    library[book_num].borrowed=0;
+    printf("성공적으로 반납했습니다. 반납한 책의 번호는 %d // 책의 이름은 %s // 책의 저자는 %s // 책의 출판사는 %s 입니다.\n",book_num,library[book_num].book_name,library[book_num].auth_name,library[book_num].publ_name);
+    return 0;
+}
+//search_book 리팩토링을 위한함수
 void search_book_helper(char (*searchInfo)[30], char * user_input,int book_total_num,int *borrowed){
     int idx;
     for(idx=0; idx<book_total_num; idx++){
@@ -76,6 +132,43 @@ void search_book_helper(char (*searchInfo)[30], char * user_input,int book_total
                 printf(" 대출됨\n");
             else
                 printf("\n");
+        }
+    }
+}
+
+void search_book_helper_struct(BookInformation * library, int user_choice, char * user_input,int book_total_num){
+    int idx=0;
+    if(user_choice==1){
+        for(idx=0; idx<book_total_num; idx++){
+            if(contain(library->book_name,user_input)){
+                printf("찾는 책의 번호 %d // 책의 이름 %s // 책의 저자 %s // 책의 출판사 %s",idx, library[idx].book_name,library[idx].auth_name,library[idx].publ_name);
+                if(library->borrowed==1)
+                    printf(" 대출됨\n");
+                else
+                    printf("\n");
+            }
+        }
+    }
+    else if(user_choice==2){
+        for(idx=0; idx<book_total_num; idx++){
+            if(contain(library->auth_name,user_input)){
+                printf("찾는 책의 번호 %d // 책의 이름 %s // 책의 저자 %s // 책의 출판사 %s",idx, library[idx].book_name,library[idx].auth_name,library[idx].publ_name);
+                if(library->borrowed==1)
+                    printf(" 대출됨\n");
+                else
+                    printf("\n");
+            }
+        }
+    }
+    else{
+        for(idx=0; idx<book_total_num; idx++){
+            if(contain(library->publ_name,user_input)){
+                printf("찾는 책의 번호 %d // 책의 이름 %s // 책의 저자 %s // 책의 출판사 %s",idx, library[idx].book_name,library[idx].auth_name,library[idx].publ_name);
+                if(library->borrowed==1)
+                    printf(" 대출됨\n");
+                else
+                    printf("\n");
+            }
         }
     }
 }
@@ -89,11 +182,29 @@ int strLen(char *str){
 int contain(char * str1, char *str2){
     int str1Len=strLen(str1);
     int str2Len=strLen(str2);
+    // printf("비교하는 문자열 %s %s",str1, str2);
     if(str1Len<str2Len)
         return 0;
     for(int i=0; i<str2Len; i++)
         if(str1[i]!=str2[i])return 0;
     return 1;
+}
+int search_book_struct(BookInformation * library,int book_total_num){
+    int user_choice;
+    char user_input[100];
+
+    printf("무슨 기준으로 찾을지 입력하세요.\n");
+    printf("1. 책의 이름 \n");
+    printf("2. 저자의 이름 \n");
+    printf("3. 출판사의 이름 \n");
+    scanf("%d",&user_choice);
+    printf("검색어를 입력하세요 : ");
+    scanf("%s",user_input);
+
+    search_book_helper_struct(library,user_choice,user_input,book_total_num);
+    
+
+    return 0;
 }
 int search_book(char (*book_name)[30], char(*auth_name)[30], char(*publ_name)[30],int * borrowed,int book_total_num){
     int user_choice;
@@ -116,6 +227,7 @@ int search_book(char (*book_name)[30], char(*auth_name)[30], char(*publ_name)[30
 
     return 0;
 }
+//구조체를 사용하지 않았을때 책을 추가하는 함수
 int add_book(char (*book_name)[30], char(*auth_name)[30], char(*publ_name)[30],int * borrowed,int * book_total_num){
     //메모리 읽기를 한번만 하기 위해서 사용하는 idx
     int idx=*book_total_num;
@@ -143,3 +255,30 @@ int add_book(char (*book_name)[30], char(*auth_name)[30], char(*publ_name)[30],i
 
     return 0;
 }
+
+int register_book(BookInformation * library,int * book_total_num){
+    int idx=*book_total_num;
+    if(idx==100){
+        printf("용량이 가득 찼습니다.\n");
+        return 0;
+    }
+
+    printf("책의 이름을 입력하세요 : ");
+    scanf("%s",library[idx].book_name);
+
+    printf("저자의 이름을 입력하세요 : ");
+    scanf("%s",library[idx].auth_name);
+
+    printf("출판사의 이름을 입력하세요 : ");
+    scanf("%s", library[idx].publ_name);
+
+    library[idx].borrowed=0;
+
+    printf("추가 성공!\n");
+    printf("추가한 책의 번호 %d // 책의 이름 %s // 책의 저자 %s // 책의 출판사 %s \n",idx,library[idx].book_name,library[idx].auth_name,library[idx].publ_name);
+    (*book_total_num)++;
+    printf("책의 총 갯수는 %d 개입니다.\n",*book_total_num);
+
+    return 0;
+}
+
